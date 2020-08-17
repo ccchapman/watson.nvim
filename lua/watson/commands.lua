6d9bdee7c61fd202708fe2726501d31a1d3df3a5
Chris Chapman <42548502+ccchapman@users.noreply.github.com>
2020-08-16 20:00:48 -0700

    feat: Add support for :WatsonStart

--- lua/watson/commands.lua
+++ lua/watson/commands.lua
@@ -11,6 +11,13 @@ local function status()
   print(Utils.exec("watson status"))
 end
 
+Api.nvim_command(
+  "com! -nargs=1 WatsonStart call luaeval('watson.start(_A)', expand('<args>'))"
+)
+local function start(project)
+  print(Utils.exec("watson start "..project))
+end
+
 Api.nvim_command("com! WatsonStop call luaeval('watson.stop()')")
 local function stop()
   print(Utils.exec("watson stop"))
@@ -24,6 +31,7 @@ end
 return {
   summary = summary,
   status = status,
+  start = start,
   stop = stop,
   restart = restart,
 }
