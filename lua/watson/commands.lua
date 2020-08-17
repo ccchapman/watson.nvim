3679dcee18ec6c5f19e98938e1dcaaf2a674d365
Chris Chapman <42548502+ccchapman@users.noreply.github.com>
2020-08-16 19:55:39 -0700

    refactor: Move Vimscript to Lua

--- lua/watson/commands.lua
+++ lua/watson/commands.lua
@@ -1,17 +1,22 @@
 local Utils = require("watson.utils")
+local Api = vim.api
 
+Api.nvim_command("com! Watson call luaeval('watson.summary()')")
 local function summary()
   print(":WatsonSummary coming soon")
 end
 
+Api.nvim_command("com! WatsonStatus call luaeval('watson.status()')")
 local function status()
   print(Utils.exec("watson status"))
 end
 
+Api.nvim_command("com! WatsonStop call luaeval('watson.stop()')")
 local function stop()
   print(Utils.exec("watson stop"))
 end
 
+Api.nvim_command("com! WatsonRestart call luaeval('watson.restart()')")
 local function restart()
   print(Utils.exec("watson restart"))
 end
