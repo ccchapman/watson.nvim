f7ae597880255f6b1675690bd92c80e45ec20f6c
Chris Chapman <42548502+ccchapman@users.noreply.github.com>
2020-08-16 18:40:59 -0700

    feat: Add :WatsonStop and :WatsonRestart

--- lua/watson/init.lua
+++ lua/watson/init.lua
@@ -1,9 +1,21 @@
+package.loaded["watson"] = nil
+
 local Utils = require("watson.utils")
 
 local function status()
   print(Utils.exec("watson status"))
 end
 
+local function stop()
+  print(Utils.exec("watson stop"))
+end
+
+local function restart()
+  print(Utils.exec("watson restart"))
+end
+
 return {
   status = status,
+  stop = stop,
+  restart = restart,
 }
--- plugin/watson.vim
+++ plugin/watson.vim
@@ -2,4 +2,14 @@ fun! WatsonStatus()
   lua require("watson").status()
 endfun
 
+fun! WatsonStop()
+  lua require("watson").stop()
+endfun
+
+fun! WatsonRestart()
+  lua require("watson").restart()
+endfun
+
 com! WatsonStatus call WatsonStatus()
+com! WatsonStop call WatsonStop()
+com! WatsonRestart call WatsonRestart()
