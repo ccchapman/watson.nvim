96658325cf597ffa5a3bc0da4bb1d17f308c259d
Chris Chapman <42548502+ccchapman@users.noreply.github.com>
2020-10-03 22:55:13 -0700

    feat: Begin summary command

--- lua/watson/commands/summary.lua
+++ lua/watson/commands/summary.lua
@@ -2,8 +2,29 @@ local Utils = require("watson.utils")
 
 Utils.api.nvim_command("com! Watson call luaeval('watson.summary()')")
 
+-- @TODO Custom file type/colors
+-- @TODO Border buffer
 local function summary()
-  print("Coming soon")
+  local width = Utils.api.nvim_get_option("columns")
+  local height = Utils.api.nvim_get_option("lines")
+  local bufWidth = math.ceil(width * 0.5)
+  local bufHeight = math.ceil(height * 0.5 - 4)
+
+  local buf = Utils.api.nvim_create_buf(false, true)
+  Utils.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+  Utils.api.nvim_open_win(buf, 0, {
+    relative = "editor",
+    width = bufWidth,
+    height = bufHeight,
+    col = math.ceil((width - bufWidth) / 2),
+    row = math.ceil((height - bufHeight) / 2 - 1),
+    style = "minimal",
+  })
+  Utils.api.nvim_put({
+    "Project: " .. Utils.exec("watson status -p"),
+    "Started: " .. Utils.exec("watson status -e"),
+  }, "", false, false)
+  Utils.api.nvim_buf_set_option(buf, "modifiable", false)
 end
 
 return summary
